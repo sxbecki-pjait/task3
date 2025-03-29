44428a4274509c5249f025066a80753e65addb1e
sxbecki-pjait <s33693@pjwstk.edu.pl>
2025-03-29 12:55:44 +0100

    reservation system completion. Main structure to be finished

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Event e1 = new Event("Ikony Motoryzacji", 80, "25th march 2025", "Warsaw");
+        ReservationSystem reservationSystem = new ReservationSystem();
+        Event e1 = new Event("Ikony Motoryzacji", 80, "25th march 2025");
         Event e2 = new Event("Ultrace", 350, "30th june 2025", "Cracow");
         Customer c1 = new Customer("Pawel","Sobecki","s33693@pjwstk.edu.pl");
+        Customer c2 = new Customer("John","Doe","jdoe@gmail.com");
+        reservationSystem.addEvent(e1,e2);
         c1.addToReservationList(e1);
         c1.addToReservationList(e2);
+        c2.addToReservationList(e1);
         c1.displayReservations();
-        c1.cancelReservation(e1);
-        c1.displayReservations();
+        Event g = reservationSystem.findEvent("Ultrace");
+        System.out.println(g.toString());
+        reservationSystem.changeEventPrice("Ultrace", 1000);
 
     }
 
--- src/ReservationSystem.java
+++ src/ReservationSystem.java
@@ -1,11 +1,62 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReservationSystem {
-    private List<Event> events;
-    private List<Customer> customers;
+    private List<Event> events = new ArrayList<>();
+    private List<Customer> customers = new ArrayList<>();
 
 // to be finished
-    public void addEvent(){
+    public void addEvent(Event event){
+        events.add(event);
+    }
+    public void addEvent(Event e1, Event e2){
+        events.add(e1);
+        events.add(e2);
+    }
+    public void addEvent(Event e1, Event e2, Event e3){
+        events.add(e1);
+        events.add(e2);
+        events.add(e3);
+    }
 
-    };
+    public void addCustomer(Customer c){
+        customers.add(c);
+    }
+    public void addCustomer(Customer c1, Customer c2){
+        customers.add(c1);
+        customers.add(c2);
+    }
+    public void addCustomer(Customer c1, Customer c2, Customer c3){
+        customers.add(c1);
+        customers.add(c2);
+        customers.add(c3);
+    }
+
+    public void makeReservation(Customer customer, Event event){
+        customer.addToReservationList(event);
+        event.reserveSeat(false);
+    }
+
+    public Event findEvent(String name){
+        for(Event e : events){
+            if(e.getName().equals(name)){
+                return e;
+            }
+        }
+        return null;
+    }
+    public Customer findCustomer(String lastName){
+        for(Customer c : customers){
+            if(c.getLastName().equals(lastName)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void changeEventPrice(String name, double newPrice){
+        findEvent(name).setPrice(newPrice);
+    }
+
+
 }
